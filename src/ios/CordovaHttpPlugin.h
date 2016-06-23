fb8fa5e2e89a49228359eb2691c7f4ff2264902f
Codin Pangell <codin@internetcowboy.org>
2016-06-23 10:29:16 -0700

    init

---
+++ src/ios/CordovaHttpPlugin.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVJSON.h>
+
+@interface CordovaHttpPlugin : CDVPlugin
+
+- (void)useBasicAuth:(CDVInvokedUrlCommand*)command;
+- (void)setHeader:(CDVInvokedUrlCommand*)command;
+- (void)enableSSLPinning:(CDVInvokedUrlCommand*)command;
+- (void)acceptAllCerts:(CDVInvokedUrlCommand*)command;
+- (void)post:(CDVInvokedUrlCommand*)command;
+- (void)postJson:(CDVInvokedUrlCommand*)command;
+- (void)get:(CDVInvokedUrlCommand*)command;
+- (void)uploadFile:(CDVInvokedUrlCommand*)command;
+- (void)downloadFile:(CDVInvokedUrlCommand*)command;
+
+@end
